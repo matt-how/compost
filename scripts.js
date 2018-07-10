e509a301ac6feb8b2a52b589d939dd88fe30ca90
Matthew Howard <matthewhoward@dyn-10-32-182-179.winet.local>
2018-07-10 14:48:03 +0100

    -Improved infinite scroll

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ window.onscroll = function() {loadContent()};
 
 function loadContent() {
   //alert("Pressed!");
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight-(document.body.offsetHeight/5)) {
         // you're at the bottom of the page
     //alert("load more")
     var listNode = document.createElement("LI");
