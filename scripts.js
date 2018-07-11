d40f9f507ed96e670c8a169d6b4c3fedb12eceb6
Matthew Howard <matthewhoward@dyn-10-32-182-179.winet.local>
2018-07-11 14:31:37 +0100

    -Posts are now loaded from the database initially

--- scripts.js
+++ scripts.js
@@ -1,25 +1,85 @@
-var text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient."
-window.onscroll = function() {loadContent()};
+//Libraries
+var config = {
+  apiKey: "AIzaSyCNtYJ9o8ks7wILKZriVjjyf_pifsVxxxM",
+  authDomain: "compost-78ddc.firebaseapp.com",
+  databaseURL: "https://compost-78ddc.firebaseio.com",
+  projectId: "compost-78ddc",
+  storageBucket: "compost-78ddc.appspot.com",
+  messagingSenderId: "401111104627"
+};
+firebase.initializeApp(config);
 
-function loadContent() {
+//when you scroll, check if you need to load more posts
+window.onscroll = function() {infiniteScroll()};
+
+//keep track of how many posts have been loaded
+var postCount = 0;
+
+function onPageLoad(){
+  var i;
+  for (i=0;i<7;i++){
+    //alert(i);
+    firebase.database().ref('posts/' + i).once('value').then(function(snapshot) {
+      document.getElementById("post-list").appendChild(returnNewPost(snapshot.val()));
+    });
+    //document.getElementById("post-list").appendChild(returnNewPost(loadPosts(i).val()));
+  }
+}
+
+/*function loadPosts(var postNum) {
+  var postsRef = firebase.database().ref('posts/' + postNum);
+  //alert(postsRef);
+  //var _this = this;
+  //return firebase.database().ref('posts/' + postNum).once('value').then(function(snapshot) {
+    //alert(snapshot.val());
+    //return(snapshot.val());
+  });
+
+}*/
+
+function returnNewPost(something){
+  var listNode = document.createElement("LI");
+  listNode.classList.add("post")
+  var divNode = document.createElement("DIV");
+  divNode.classList.add("col-10");
+  divNode.classList.add("text");
+  var textNode = document.createTextNode(something);
+  divNode.appendChild(textNode);
+  listNode.appendChild(divNode);
+  var sideDiv = document.createElement("DIV");
+  sideDiv.classList.add("col-2");
+  sideDiv.classList.add("side");
+  var testText = document.createTextNode("test");
+  sideDiv.appendChild(testText);
+  listNode.appendChild(sideDiv);
+  return listNode;
+}
+
+function returnFakePost(){
+  var listNode = document.createElement("LI");
+  listNode.classList.add("post")
+  var divNode = document.createElement("DIV");
+  divNode.classList.add("col-10");
+  divNode.classList.add("text");
+  var textNode = document.createTextNode("This is a fake post. Nobody posts like that.");
+  divNode.appendChild(textNode);
+  listNode.appendChild(divNode);
+  var sideDiv = document.createElement("DIV");
+  sideDiv.classList.add("col-2");
+  sideDiv.classList.add("side");
+  var testText = document.createTextNode("test");
+  sideDiv.appendChild(testText);
+  listNode.appendChild(sideDiv);
+  return listNode;
+}
+
+
+function infiniteScroll() {
   //alert("Pressed!");
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight-(document.body.offsetHeight/5)) {
         // you're at the bottom of the page
     //alert("load more")
-    var listNode = document.createElement("LI");
-    listNode.classList.add("post")
-    var divNode = document.createElement("DIV");
-    divNode.classList.add("col-10");
-    divNode.classList.add("text");
-    var textNode = document.createTextNode(text);
-    divNode.appendChild(textNode);
-    listNode.appendChild(divNode);
-    var sideDiv = document.createElement("DIV");
-    sideDiv.classList.add("col-2");
-    sideDiv.classList.add("side");
-    var testText = document.createTextNode("test");
-    sideDiv.appendChild(testText);
-    listNode.appendChild(sideDiv);
+    var listNode = returnFakePost();
     document.getElementById("post-list").appendChild(listNode);
   }
 }
